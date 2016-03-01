82b7af56763980ba012996f6edf3be561ca99cfc
Adrian <a.whole.dev@gmail.com>
2016-03-01 01:11:22 +0100

    Added Bridge

---
+++ Scoops/Scoops-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+// Bridbging - Headers for Windows Azure Mobile Services
+#import <WindowsAzureMobileServices/WindowsAzureMobileServices.h>
